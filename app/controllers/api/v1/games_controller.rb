04d3760a5940891233b42c89363dcf94da9c05d1
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-15 11:16:22 -0600

    user can create a game

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,12 +2,17 @@ module Api
   module V1
     class GamesController < ActionController::API
       def create
-        player_1 = request.headers['X-Api-KEY']
-        # player_2 = headers, email confirm active status
-        # player_1_board = Board.new
-        # player_2_board = Board.new
-        #
-        # game = Game.create(^^^)
+        player_1 = User.find_by_apikey(request.headers['X-Api-KEY'])
+        if User.find_by_email(params['opponent_email']).active?
+          player_2 = User.find_by_email(params['opponent_email'])
+        else
+          flash[:error] = "Sorry, #{params['opponent_email']} hasn't activated their account"
+          redirect_to root
+        end
+        player_1_board = Board.new(4)
+        player_2_board = Board.new(4)
+
+        game = Game.create(player_1_board: player_1_board, player_2_board: player_2_board)
         render json: game
       end
 
