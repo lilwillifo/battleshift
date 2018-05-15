2670dd994d565d33857e78e069d2997c03d37207
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-15 10:47:24 -0600

    pseudocoding create game

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,6 +1,16 @@
 module Api
   module V1
     class GamesController < ActionController::API
+      def create
+        player_1 = request.headers['X-Api-KEY']
+        # player_2 = headers, email confirm active status
+        # player_1_board = Board.new
+        # player_2_board = Board.new
+        #
+        # game = Game.create(^^^)
+        render json: game
+      end
+
       def show
         game = Game.find(params[:id])
         render json: game
