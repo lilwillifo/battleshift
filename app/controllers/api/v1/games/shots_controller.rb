e364a4172ac39305faf8f71c8d77c3b30d727579
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-17 14:53:22 -0600

    building controller logic for current turn, but current turn isnt changing yet

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -1,14 +1,19 @@
-module Api
-  module V1
-    module Games
-      class ShotsController < ApiController
-        def create
-          turn_processor = TurnProcessor.new(current_game, params[:shot][:target], current_player, current_opponent)
+class Api::V1::Games::ShotsController < ApiController
+  def create
+    if players[request.headers['X-API-KEY']] != current_game.current_turn
+      render json: {status: 400}
+    else
+      turn_processor = TurnProcessor.new(current_game, params[:shot][:target], current_player, current_opponent)
 
-          turn_processor.run!
-          render json: current_game, message: turn_processor.message
-        end
-      end
+      turn_processor.run!
+      render json: current_game, message: turn_processor.message
     end
+  end
+
+  private
+
+  def players
+    {current_game.player_1.api_key => 'player_1',
+      current_game.player_2.api_key => 'player_2'}
   end
 end
