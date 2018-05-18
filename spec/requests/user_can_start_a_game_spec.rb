f011abcfeee6abb48558dc8b3d07d2dc057d0c4d
Megan Arellano <ssciolist@gmail.com>
2018-05-17 20:24:43 -0600

    adds test for p2 activation, changes game controller

--- spec/requests/user_can_start_a_game_spec.rb
+++ spec/requests/user_can_start_a_game_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe 'POST /api/v1/games' do
 
   context 'with two active players' do
-    let(:user) { User.create!(name: 'Jimmy', email: 'jimmy@jimmyhouse.com', password: 'justjimmythings', apikey: SecureRandom.hex)}
 
     it 'starts a game with boards' do
+      user = User.create!(name: 'Jimmy', email: 'jimmy@jimmyhouse.com', password: 'justjimmythings', apikey: SecureRandom.hex)
       User.create!(name: 'Adam', email: 'adam@bballbois.com', password: 'adamadamadam', apikey: SecureRandom.hex).active!
 
       headers = { "CONTENT_TYPE" => "application/json", "X-API-KEY" => user.apikey }
@@ -30,25 +30,18 @@ describe 'POST /api/v1/games' do
 
   context 'with one inactive player' do
     it 'returns a 400' do
-      User.create!(name: 'Adam', email: 'adam@bballbois.com', password: 'adamadamadam', apikey: SecureRandom.hex)
+      user = User.create!(name: 'Jimmy', email: 'jimmy@jimmyhouse.com', password: 'justjimmythings', apikey: SecureRandom.hex)
+      opponent = User.create!(name: 'Adam', email: 'adam@bballbois.com', password: 'adamadamadam', apikey: SecureRandom.hex)
 
       headers = { "CONTENT_TYPE" => "application/json", "X-API-KEY" => user.apikey }
-      email = {opponent_email: "adam@bballbois.com"}.to_json
+      email = {opponent_email: "#{opponent.email}"}.to_json
 
       post "/api/v1/games", params: email, headers: headers
 
       actual  = JSON.parse(response.body, symbolize_names: true)
-      expected = Game.last
 
-      expect(response).to be_success
-      expect(actual[:id]).to eq(expected.id)
-      expect(actual[:current_turn]).to eq(expected.current_turn)
-      expect(actual[:player_1_board][:rows].count).to eq(4)
-      expect(actual[:player_2_board][:rows].count).to eq(4)
-      expect(actual[:player_1_board][:rows][0][:name]).to eq("row_a")
-      expect(actual[:player_1_board][:rows][3][:data][0][:coordinates]).to eq("D1")
-      expect(actual[:player_1_board][:rows][3][:data][0][:coordinates]).to eq("D1")
-      expect(actual[:player_1_board][:rows][3][:data][0][:status]).to eq("Not Attacked")
+      expect(response).to have_http_status(400)
+      expect(actual[:error]).to eq("Sorry, #{opponent.email} hasn't activated their account")
 
     end
   end
