3bbd7718ccac3a48798dcda19ccf9c75c3e6aa1e
Megan Arellano <ssciolist@gmail.com>
2018-05-19 17:24:49 -0600

    finishes turn_processor_spec using doubles

--- spec/services/turn_processor_spec.rb
+++ spec/services/turn_processor_spec.rb
@@ -9,15 +9,19 @@ describe TurnProcessor do
 
   let(:game) { Game.new(player_1: player_1, player_2: player_2)}
 
-  subject { TurnProcessor.new(game, "A4", board, player_1)}
+  subject { TurnProcessor.new(game, "A4", player_1, player_2)}
 
-  it "exists when provided a game, target, board and player" do
-    expect(subject).to be_a TurnProcessor
+  describe "class_methods" do
+    context "initialize" do
+      it "exists when provided a game, target, board and player" do
+        expect(subject).to be_a TurnProcessor
+      end
+    end
   end
 
   describe "instance_methods" do
     context "#run!" do
-      it "does a turn" do
+      it "changes turns and sends a message" do
         expect(game.current_turn).to eq("player_1")
 
         allow_any_instance_of(Shooter).to receive(:fire!).and_return('Your shot resulted in a Miss')
@@ -28,7 +32,19 @@ describe TurnProcessor do
         expect(game.current_turn).to eq("player_2")
         expect(subject.message).to eq('Your shot resulted in a Miss')
       end
+
+      it "can assign a winner" do
+        expect(game.current_turn).to eq("player_1")
+
+        allow_any_instance_of(Shooter).to receive(:fire!).and_return("Battleship sunk.")
+        allow(board).to receive(:defeated?).and_return(true)
+
+        subject.run!
+        expect(game.winner).to eq("#{user.email}")
+
+        expect(subject.message).to eq("Battleship sunk. Game over.")
+        expect(subject.status).to eq(200)
+      end
     end
   end
-
 end
