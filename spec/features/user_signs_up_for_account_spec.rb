bac7f3c8ce0d91ea41c9247418675b5ed104d440
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-20 10:28:58 -0600

    testing for registration sad path

--- spec/features/user_signs_up_for_account_spec.rb
+++ spec/features/user_signs_up_for_account_spec.rb
@@ -22,5 +22,20 @@ describe 'As a guest user' do
       expect(page).to have_content "Logged in as Josh"
       expect(page).to have_content "This account has not yet been activated. Please check your email."
     end
+    it "I can't register for account if passwords don't match" do
+      visit "/"
+      click_on "Register"
+
+      expect(current_path).to eq "/register"
+
+      fill_in "user[email]", with: 'ilanarox@fake.com'
+      fill_in "user[name]", with: 'Josh'
+      fill_in "user[password]", with: 'vErYsEcUrE'
+      fill_in "user[password_confirmation]", with: 'whoops'
+      click_on "Submit"
+
+      expect(current_path).to eq "/register"
+      expect(page).to have_content("Sorry, please try again.")
+    end
   end
 end
