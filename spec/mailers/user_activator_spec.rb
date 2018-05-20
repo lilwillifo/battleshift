c5532d9be041e6e5989baee781ce0745027433e5
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-20 09:55:34 -0600

    updating mailer spec to use fixtures

--- spec/mailers/user_activator_spec.rb
+++ spec/mailers/user_activator_spec.rb
@@ -1,22 +1,27 @@
 require "rails_helper"
 
 RSpec.describe UserActivatorMailer, type: :mailer do
-  it "sends an email" do
-    # Create the email and store it for further assertions
-    user = User.create(name: 'Megan', email: 'kewlkid@hotmail.com', password: 'gIrLsRuLeBoYsDrOOl', apikey: ENV['USER_KEY'])
-    email = UserActivatorMailer.welcome_email(user)
-    # Send the email, then test that it got queued
-    email.deliver_now
-    expect(ActionMailer::Base.deliveries.last).to eq(email)
+  context 'sends activation email' do
+    let(:user) { User.create(id: 3000, name: 'Megan', email: 'kewlkid@hotmail.com', password: 'gIrLsRuLeBoYsDrOOl') }
+    let(:mail) { UserActivatorMailer.welcome_email(user) }
 
-    expect(email.from).to eq(["app96560814@heroku.com"])
-    expect(email.to).to eq(['kewlkid@hotmail.com'])
-    expect(email.subject).to eq('Welcome to BattleShift, Megan')
-    
-    expect(email.html_part.body.to_s).to have_content("Visit here to activate your account.")
-    expect(email.html_part.body.to_s).to have_content("Here's your api key:")
-    key = email.html_part.body.raw_source.match(/key: (?<key>.+?)\n/)[:key]
-    expect(key.length).to eq(32)
-    expect(email.html_part.body.to_s).to include(key)
+    # before(:each) do
+    #   allow_any_instance_of(User).to receive(:api_key).and_return("bb609b650e6022e0b5fbd1fae290b5e9")
+    # end
+
+    it 'loads the email' do
+      expect { mail.deliver_now }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+
+    it "renders the headers and body" do
+      expect(mail.subject).to eq("Welcome to BattleShift, #{user.name}")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["app96560814@heroku.com"])
+
+      text_body = File.read('spec/fixtures/user_activator_mailer/activate.txt')
+      html_body = File.read('spec/fixtures/user_activator_mailer/activate.html')
+      expect(mail.text_part.body.to_s.chomp).to match(text_body)
+      expect(mail.html_part.body.to_s).to match(html_body)
+    end
   end
 end
